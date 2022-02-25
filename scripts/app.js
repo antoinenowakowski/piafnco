fe389af4bcddc36628fdfcc11cf8ece9fc8e8b2a
antoinenowakowski <70375463+antoinenowakowski@users.noreply.github.com>
2022-02-26 00:06:02 +0100

    js delete img

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+// replace a class 
+if (outerWidth < 1560) {
+    console.log(outerWidth)
+    console.log(document.querySelector(' div.img-entry').className="img-entry-second"); 
+} else {
+    console.log("no !")
+}
+
