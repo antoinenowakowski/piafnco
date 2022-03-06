b505723493b5614590d7304093e9f8ef52f21170
antoinenowakowski <70375463+antoinenowakowski@users.noreply.github.com>
2022-03-06 20:14:13 +0100

    change all the design; first part + product

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,1 @@
-// replace a class 
-if (outerWidth < 1560) {
-    console.log(outerWidth, "largeur")
-    console.log(document.querySelector(' section > div.image-entry').className="img-entry-second"); 
-} else {
-    console.log("no !")
-}
 
