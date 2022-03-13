71890b73e747fbd422e05ce29f8a0be21d409870
antoinenowakowski <70375463+antoinenowakowski@users.noreply.github.com>
2022-03-13 12:08:10 +0100

    slider

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,30 @@
+// les images doivent toutes êtres stockées dans le même dossier dans une meme liste
 
+var listeOfAllImages = ["assets/Imgs/Carrousel/IMG1.png", "assets/Imgs/Carrousel/IMG2.png", "assets/Imgs/Carrousel/IMG3.png", "assets/Imgs/Carrousel/IMG4.png", "assets/Imgs/Carrousel/IMG5.png", "assets/Imgs/Carrousel/IMG6.png", "assets/Imgs/Carrousel/IMG7.png", "assets/Imgs/Carrousel/IMG8.png",]
+var pointeur = 0;
+var images = document.querySelector("#contain-img");
+let init = images.style.backgroundImage = "url(" + listeOfAllImages[pointeur] + ")"; //init image one
+
+
+function ChangeImageRight(){
+    pointeur++;
+    console.log(pointeur);
+    images.style.backgroundImage = "url(" + listeOfAllImages[pointeur] + ")"; //change le bg 
+
+    if (pointeur > listeOfAllImages.length-2) { // retourne au début du slide
+        pointeur = -1;
+        init;
+    } else {
+    }
+}
+function ChangeImageLeft(){
+    pointeur--;
+    console.log(pointeur);
+    images.style.backgroundImage = "url(" + listeOfAllImages[pointeur] + ")";
+
+    if (pointeur <= -1) { 
+        pointeur = listeOfAllImages.length-1
+        images.style.backgroundImage = "url(" + listeOfAllImages[pointeur] + ")";
+    } else {
+    }
+}
