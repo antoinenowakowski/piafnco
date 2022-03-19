f74dd479f31cdc4b09c106e9410d8cab2f308b03
antoinenowakowski <70375463+antoinenowakowski@users.noreply.github.com>
2022-03-19 12:09:12 +0100

    final website, responsive ok, js ok

--- scripts/app.js
+++ scripts/app.js
@@ -1,30 +1,31 @@
 // les images doivent toutes êtres stockées dans le même dossier dans une meme liste
 
-var listeOfAllImages = ["assets/Imgs/Carrousel/IMG1.png", "assets/Imgs/Carrousel/IMG2.png", "assets/Imgs/Carrousel/IMG3.png", "assets/Imgs/Carrousel/IMG4.png", "assets/Imgs/Carrousel/IMG5.png", "assets/Imgs/Carrousel/IMG6.png", "assets/Imgs/Carrousel/IMG7.png", "assets/Imgs/Carrousel/IMG8.png",]
-var pointeur = 0;
-var images = document.querySelector("#contain-img-slider");
-let init = images.style.backgroundImage = "url(" + listeOfAllImages[pointeur] + ")"; //init image one
+let listeOfAllImages = ["assets/Imgs/Carrousel/IMG1.png", "assets/Imgs/Carrousel/IMG2.png", "assets/Imgs/Carrousel/IMG3.png", "assets/Imgs/Carrousel/IMG4.png", "assets/Imgs/Carrousel/IMG5.png", "assets/Imgs/Carrousel/IMG6.png", "assets/Imgs/Carrousel/IMG7.png", "assets/Imgs/Carrousel/IMG8.png",]
+let pointeur = 0; // initialise le pointeur
+let images = document.querySelector("#contain-img-slider"); // get l'emplacement du slider
+let init = images.style.backgroundImage = "url(" + listeOfAllImages[pointeur] + ")"; //init image une
 
 
 function ChangeImageRight(){
-    pointeur++;
-    console.log(pointeur);
+    pointeur++; // incrémente le pointeur de 1
     images.style.backgroundImage = "url(" + listeOfAllImages[pointeur] + ")"; //change le bg 
-
-    if (pointeur > listeOfAllImages.length-2) { // retourne au début du slide
-        pointeur = -1;
-        init;
+    
+    if (pointeur == 8) {
+        images.style.backgroundImage = "url(" + listeOfAllImages[pointeur=0] + ")";
     } else {
+
     }
+
 }
 function ChangeImageLeft(){
-    pointeur--;
-    console.log(pointeur);
-    images.style.backgroundImage = "url(" + listeOfAllImages[pointeur] + ")";
+    pointeur--; // décrémente le pointeur de 1
+    images.style.backgroundImage = "url(" + listeOfAllImages[pointeur] + ")"; //change le background
 
-    if (pointeur <= -1) { 
-        pointeur = listeOfAllImages.length-1
-        images.style.backgroundImage = "url(" + listeOfAllImages[pointeur] + ")";
+    if (pointeur == -1) {
+        images.style.backgroundImage = "url(" + listeOfAllImages[pointeur=7] +")";
     } else {
+
     }
+
 }
+
