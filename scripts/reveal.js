392e833d7c4b9e9cbe6c8c212de4a07dc0297a2b
antoinenowakowski <70375463+antoinenowakowski@users.noreply.github.com>
2022-04-08 22:36:01 +0200

    final version

---
+++ scripts/reveal.js
@@ -0,0 +1,103 @@
+const sr = ScrollReveal();
+
+sr.reveal('.contain-motto-text', {
+    delay: 325, 
+    origin: 'top', 
+    duration: 850, 
+    distance: '60px', 
+    scale: 0.6
+});
+sr.reveal('.contain-text-presentation', {
+    delay: 550, 
+    origin: 'top', 
+    duration: 700,
+    distance: '50px', 
+    scale: 0.8
+})
+sr.reveal('.container-button-buy', {
+    delay: 350, 
+    origin: 'top', 
+    duration: 600, 
+    distance: '50px', 
+    scale: 0.9
+})
+
+
+sr.reveal('.contain-title-product', {
+    origin: 'bottom',
+    duration: 500, 
+    distance: '30px', 
+    delay: 700
+})
+sr.reveal('.container-title-imgs', {
+    origin: 'bottom', 
+    duration: 500, 
+    distance: '50px', 
+    delay: 650, 
+    scale: 0.8  
+})
+
+
+sr.reveal('.icon-people', {
+    origin: 'left', 
+    distance: '40px', 
+    duration: 600, 
+    delay: 350  
+})
+sr.reveal('.service-title-and-description', {
+    origin: 'top', 
+    distance: '200px', 
+    duration: 350, 
+    delay: 1200
+})
+
+
+sr.reveal('.title-partenaires', {
+    origin: 'top', 
+    delay: 350, 
+    distance: '60px',
+    duration: 400
+})
+sr.reveal('.container-images-partenaires', {
+    origin: 'bottom', 
+    duration: 600, 
+    delay: 450, 
+    distance: '60px', 
+    scale: 0.6
+})
+
+
+sr.reveal('.title-speech', {
+    origin: 'top', 
+    duration: 600, 
+    distance: '70px',
+    delay: 350, 
+    scale: 0.8
+})
+sr.reveal('.contain-video-speech', {
+    origin: 'bottom', 
+    duration: 700, 
+    distance: '80px', 
+    delay: 450
+})
+sr.reveal('.button-vote', {
+    origin: 'top', 
+    delay: 550, 
+    duration: 450, 
+    distance: '120px', 
+    scale: 0
+})
+
+
+sr.reveal('.contain-title-us', {
+    origin: 'top', 
+    duration: 600, 
+    distance: '60px', 
+    delay: 350
+})
+sr.reveal('.slider', {
+    origin: 'bottom', 
+    duration: 700, 
+    distance: '80px', 
+    delay: 350, 
+})
